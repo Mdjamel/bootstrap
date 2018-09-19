b9d34e6641d329a933ed764a00207b41fca06d10
mdjamel <mdjaich@gmail.com>
2018-09-19 16:04:12 +0200

    5th update

--- js/plugins.js
+++ js/plugins.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
   $(".carousel").carousel({
     interval: 6000
   });
+
+  /* Show Color Optio Dic When Click On The Gear*/
+
+  $(".gearcheck").click(function() {
+    $(".coloroption").fadeToggle();
+  });
 });
